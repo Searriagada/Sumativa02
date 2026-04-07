4aa604acd866f6199119f273f7a0667894a2eff2
Sebastian Arriagada <sebastian.arriag93@gmail.com>
2026-04-06 21:00:24 -0400

    a

---
+++ src/main/java/com/programacionavanzada/sumativa02/Sumativa02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.programacionavanzada.sumativa02;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Sumativa02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
