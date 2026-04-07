ada3a9c7fbcf0673fe852a700b6a7e35b9a22145
1268Javier <j.andresvargasduran@gmail.com>
2026-04-07 11:25:16 -0400

    revisar lo avanzado

--- src/main/java/com/programacionavanzada/sumativa02/RegistroUsuarios.java
+++ src/main/java/com/programacionavanzada/sumativa02/RegistroUsuarios.java
@@ -1,13 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.programacionavanzada.sumativa02;
 
-/**
- *
- * @author PROFESORES 18
- */
+import java.util.ArrayList;
+
 public class RegistroUsuarios {
+    private ArrayList<Usuario> listaUsuarios; 
+
+    public RegistroUsuarios() {
+        this.listaUsuarios = new ArrayList<>(); 
+    }
+    
+    // CUSTOMERS
+    // Metodo buscar
+    public boolean buscar (String numeroRut) {
+        for (Usuario i : listaUsuarios) {
+            if(numeroRut.equalsIgnoreCase(i.getNumeroRut())) {
+                return true; 
+            }
+        }
+        return false; 
+    }
+    
+    // Método agregar 
+    public void agregar (Usuario usuario) {
+            if (buscar(usuario.getNumeroRut())) {
+                System.out.println("Usuario ya registrado");
+            }
+            else{
+                listaUsuarios.add(usuario); 
+                System.out.println("Usuario ingresado correctamente");
+            }
+        }
+
+    // Método mostrar
+    public void mostrar (){
+        for (Usuario i : listaUsuarios) {
+            System.out.println(i);
+        }
+    }
+    
+    // Método eliminar 
+    public boolean eliminar (String numeroRut) {
+        for (Usuario i : listaUsuarios) {
+            if (numeroRut.equalsIgnoreCase(i.getNumeroRut())){
+                listaUsuarios.remove(i);
+                return true;
+            }
+        }
+        return false; 
+    }
+
+    
+    
     
 }
