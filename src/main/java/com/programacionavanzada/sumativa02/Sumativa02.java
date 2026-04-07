2625f980c7614f78dcec4d5a42bf467a1e58d3aa
1268Javier <j.andresvargasduran@gmail.com>
2026-04-06 22:02:35 -0400

    Escribi en la linea 2

--- src/main/java/com/programacionavanzada/sumativa02/Sumativa02.java
+++ src/main/java/com/programacionavanzada/sumativa02/Sumativa02.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.programacionavanzada.sumativa02;
 
-/**
- *
- * @author Usuario
- */
 public class Sumativa02 {
 
     public static void main(String[] args) {
