86de3d57751922399860f434e12b5ef9963c3cba
Sebastian Arriagada <sebastian.arriag93@gmail.com>
2026-04-07 23:35:21 -0400

    Libro ok, crear y eliminar libro en Biblioteca ok

--- src/main/java/com/programacionavanzada/sumativa02/Biblioteca.java
+++ src/main/java/com/programacionavanzada/sumativa02/Biblioteca.java
@@ -1,6 +1,39 @@
-
 package com.programacionavanzada.sumativa02;
+import java.util.ArrayList;
 
 public class Biblioteca {
+    
+    private ArrayList<Libro> listaLibros = new ArrayList<>();
+    
+    /**
+     * método para agregar libros, si el isbn ya existe no se agrega a lista
+     * @param libro  que será agregado
+     * @return  true si fue agregado exitosamente, false si el isbn ya existe
+     */
+    public boolean agregarLibro(Libro libro){
+        for(Libro i : listaLibros){
+            if(i.getIsbn().equals(libro.getIsbn())){
+                System.out.println("El isbn ya existe");
+                return false;
+            }
+        }
+        listaLibros.add(libro);
+        return true;
+    }
+    /**
+     * método para eliminar un libro, valida que el isbn exista antes de eliminar el objeto
+     * @param isbn del libro a eliminar
+     * @return true si el libro existe y fue removido, false si el isbn no existe
+     */
+    public boolean eliminarLibro(String isbn){
+        for(Libro i : listaLibros){
+            if(i.getIsbn().equals(isbn)){
+                listaLibros.remove(i);
+                return true;
+            }
+        }
+        System.out.println("El isbn no existe");
+        return false;
+    }
     
 }
---
+++ src/main/java/com/programacionavanzada/sumativa02/Libro.java
@@ -0,0 +1,93 @@
+package com.programacionavanzada.sumativa02;
+
+/**
+ *
+ * @author 
+ */
+public class Libro {
+    
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private int stockBiblioteca;
+    private int stockDisponible;
+    private String urlImagen; 
+    
+    public Libro(){}
+
+    public Libro(String isbn, String titulo, String autor, int stockBiblioteca, int stockDisponible, String urlImagen) {
+        setIsbn(isbn);
+        setTitulo(titulo);
+        setAutor(autor);
+        setStockBiblioteca(stockBiblioteca);
+        setStockDisponible(stockDisponible);
+        setUrlImagen(urlImagen);
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getStockBiblioteca() {
+        return stockBiblioteca;
+    }
+
+    public int getStockDisponible() {
+        return stockDisponible;
+    }
+
+    public String getUrlImagen() {
+        return urlImagen;
+    }
+
+    public void setIsbn(String isbn) {
+        if(isbn == null || isbn.isBlank()){
+            throw new IllegalArgumentException("El isbn no puede ser nulo y debe contener texto");
+        }
+        this.isbn = isbn;
+    }
+
+    public void setTitulo(String titulo) {
+        if(titulo == null || titulo.isBlank()){
+            throw new IllegalArgumentException("El título no puede ser nulo y debe contener texto");
+        }
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        if(autor == null || autor.isBlank()){
+            throw new IllegalArgumentException("El autor no puede ser nulo y debe contener texto");
+        }
+        this.autor = autor;
+    }
+
+    public void setStockBiblioteca(int stockBiblioteca) {
+        if(stockBiblioteca <= 0){
+            throw new IllegalArgumentException("El stock debe ser mayor o igual a 1");
+        }
+        this.stockBiblioteca = stockBiblioteca;
+    }
+
+    public void setStockDisponible(int stockDisponible) {
+        if(stockDisponible <= 0 || stockDisponible > stockBiblioteca){
+            throw new IllegalArgumentException("El stock debe ser mayor o igual a 1 y no debe exceder la cantidad en biblioteca");
+        }
+        this.stockDisponible = stockDisponible;
+    }
+
+    public void setUrlImagen(String urlImagen) {
+        if(urlImagen == null || urlImagen.isBlank()){
+            this.urlImagen = "http://bibliotecaunab.cl/sample.png";
+        }else{
+            this.urlImagen = urlImagen;
+        }
+    }
+}
